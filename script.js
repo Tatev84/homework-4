c1a3b9c78badff73ecf5c9f79b2168744ce5f85d
Tatev84 <tatevh84@gmail.com>
2021-07-14 00:20:49 +0400

    new homework

---
+++ script.js
@@ -0,0 +1,92 @@
+/*  let people = ["Greg", "Mary", "Devon", "James"]
+    for(var i=0; i<people.length; i++){
+    console.log(people[i])
+}  
+ */
+
+/* let people = ["Greg", "Mary", "Devon", "James"]
+    people.shift()
+    console.log(people) */
+
+
+ /* let people = ["Greg", "Mary", "Devon", "James"]
+    people.pop()
+    console.log(people)  */
+/* 
+    let people = ["Greg", "Mary", "Devon", "James"]
+        people.unshift("Matt")
+        console.log(people)  */
+/* 
+     let people = ["Greg", "Mary", "Devon", "James"]
+     people.push("Tatev")
+     console.log(people)  */
+
+     /* let people = ["Greg", "Mary", "Devon", "James"]
+     var s =people.includes("Mary")
+     console.log(s)  */
+
+
+     /*  let people = ["Greg", "Mary", "Devon", "James"]
+      let s = people.splice(1,1)
+       console.log(s)  
+       console.log(people)*/  
+  
+      /*  let people = ["Greg", "Mary", "Devon", "James"]
+       var s = people.indexOf("Mary")
+       console.log(s)  */
+
+     /*   let people = ["Greg", "Mary", "Devon", "James"]
+             var s = people.indexOf("Foo")
+             console.log(s) */
+/* 
+     let people = ["Greg", "Mary", "Devon", "James"]
+        var s= people.splice(2,1, "Elizabeth", "Atie");
+        console.log(s);
+        console.log(people)  */
+ 
+
+      /*   let people = ["Greg", "Mary", "Devon", "James"]
+
+        var people1=["Bob"]
+        var people2=people.concat(people1)
+        console.log(people2) */
+/* 
+        var arr=[];
+        var min=40;
+        var max=600;
+    for(var i=min; i<max; i++){
+        if(i%11===0){
+        arr.push(i)
+            console.log([i])
+        }
+    }  */
+/* 
+    var arr=[];
+    var a=100;
+    var b=900;
+
+    for (var i=a; i<b; i++){
+    if(i%13===0){
+        arr.push(i)
+        console.log([i])
+    }
+
+    } */
+/*  
+    var arr=[1,5,true,0,"hello",false,-5,"world",88]
+    for(var i =0; i<arr.length; i++){
+        if (typeof arr[i]==="number")
+        console.log(arr[i]) 
+
+} */
+
+   /*  var arr=[1,5,true,0,"hello",false,-5,"world",88]
+    for(var i =0; i<arr.length; i++){
+        if (typeof arr[i]==="string")
+        console.log(arr[i])
+    } 
+
+/*  var arr=[1,5,true,0,"hello",false,-5,"world",88]
+    for(var i =0; i<arr.length; i++){
+        if (typeof arr[i]==="boolean")
+        console.log(arr[i])  */
